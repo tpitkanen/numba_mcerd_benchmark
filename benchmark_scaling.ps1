ba95757305655574e0aa1a386b7cb0682b266bb9
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2022-02-25 19:09:23 +0200

    Add scaling test

---
+++ benchmark_scaling.ps1
@@ -0,0 +1,49 @@
+Import-Module (Join-Path -Path $PSScriptRoot -ChildPath .\utils.psm1)
+
+function Measure-Cores {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)]
+        [int]$CoreCount
+    )
+
+    Write-Output "`nMeasure-Cores ($CoreCount) starting`n"
+
+    $configFile = '.\config.py'
+    $originalConfig = Get-Content -Path $configFile
+    $newSettings = $originalConfig -replace 'PARALLEL_THREAD_COUNT =.*', "PARALLEL_THREAD_COUNT = ${CoreCount}"
+
+    Remove-Pycache
+    try {
+        Set-Content -Path $configFile -Value $newSettings
+        python main_jit_mt.py 2>&1 > "out_jit_mt_${CoreCount}_first.txt"
+        python main_jit_mt.py 2>&1 > "out_jit_mt_${CoreCount}_second.txt"
+        python main_jit_mt.py 2>&1 > "out_jit_mt_${CoreCount}_third.txt"
+    }
+    finally {
+        Set-Content -Path $configFile -Value $originalConfig
+    }
+    Remove-Pycache
+
+    Write-Output "`nMeasure-Cores (${CoreCount}) done`n"
+}
+
+function Invoke-Measurements {
+    Push-Location
+
+    Set-Location 'c:\kurssit\gradu\koodi\numba_mcerd\'
+    .\env\Scripts\activate
+
+    $env:PYTHONPATH = (Get-Location).Path
+    Set-Location numba_mcerd
+
+    Measure-Cores -CoreCount 1
+    Measure-Cores -CoreCount 2
+    Measure-Cores -CoreCount 4
+    Measure-Cores -CoreCount 8
+    Measure-Cores -CoreCount 12
+    Measure-Cores -CoreCount 20
+
+    Pop-Location
+}
+Invoke-Measurements
