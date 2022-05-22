2d65f7070567724a2fd77bb9cf22b66ae09f6ff1
Tuomas Pitkänen <55845778+tpitkanen@users.noreply.github.com>
2022-05-22 17:42:12 +0300

    Add try-finally to run_numba_mcerd

--- run_numba_mcerd.ps1
+++ run_numba_mcerd.ps1
@@ -1,15 +1,18 @@
 function Invoke-NumbaMcerd {
     Push-Location
+
+    try {
+        Set-Location 'c:\kurssit\gradu\koodi\numba_mcerd\'
+        .\env\Scripts\activate
     
-    Set-Location 'c:\kurssit\gradu\koodi\numba_mcerd\'
-    .\env\Scripts\activate
+        $env:PYTHONPATH = (Get-Location).Path
+        Set-Location numba_mcerd
     
-    $env:PYTHONPATH = (Get-Location).Path
-    Set-Location numba_mcerd
-    
-    python main_jit.py
-    
-    Pop-Location
+        python main_jit.py
+    }
+    finally {
+        Pop-Location
+    }
 }
 
 Invoke-NumbaMcerd
